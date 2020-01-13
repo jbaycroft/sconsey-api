b395efd38f6996ad771b47a17d4382b8ed22d494
John Baycroft <john@sconseycider.com>
2020-01-13 17:03:58 -0500

    added logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,14 +35,14 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-s3`,
       options: {
-        bucketName: "sconsey-front",
+        bucketName: "www.sconseycider.com",
       },
     },
   ],
